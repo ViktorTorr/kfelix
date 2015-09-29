4eed2311d149ad3a7d416ced9635f9c85a7f0481
ViktorTorr <viktor.torr@gmail.co>
2015-09-30 00:55:42 +0200

    project change

---
+++ src/app.js
@@ -0,0 +1,7 @@
+angular.module('app', ['ngNewRouter'])
+  .controller('AppController', ['$router', AppController]);
+
+AppController.$routeConfig([
+  {path: '/', component: 'home' }
+]);
+function AppController ($router) {}
